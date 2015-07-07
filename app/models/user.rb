426624526780a598fb7290956d22b5bcc38b3018
Leon Zagorouiko <Zagorouiko@gmail.com>
2015-07-07 15:42:17 -0700

    add questions, users, and sessions; controllers, views, and models.

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  attr_accessor :password
+
+  validates :email, :password, :username, :presence => true
+  validates_confirmation_of :password
+  validates_uniqueness_of :email
+  before_save :encrypt_password
+  after_create :send_signup_confirmation
+  has_many :questions
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def self.authenticate(email, password)
+    user = User.where(email: email).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def send_signup_confirmation
+    UserMailer.signup_confirmation(self).deliver_now
+  end
+end
